d50d5569d87000c603fea04844519dfedbad92d9
capmars <capmars@gmail.com>
2023-05-04 17:12:11 +0200

    Big Branch

--- src/VeryNiceTree.scala
+++ src/VeryNiceTree.scala
@@ -8,38 +8,57 @@ trait TreeDrawer{
 
   def drawBase(d: Double): Unit = {
     myLittleTurtle.setColor(Color.BLACK)
+    myLittleTurtle.setWidth(5)
     myLittleTurtle.forward(d)
   }
 
   def drawBranch(d: Double): Unit = {
-    myLittleTurtle.setColor(Color.GREEN)
-    myLittleTurtle.forward(d)
+    if (d > 1) {
+      myLittleTurtle.setColor(Color.PINK)
+      val anAngleSaver = myLittleTurtle.getTurtleAngle
+      val aPositionSaver = myLittleTurtle.getPosition
+      myLittleTurtle.turn(Random.between(-360, 360))
+      //left subtree
+      drawBranch(d - 1)
+
+      myLittleTurtle.setAngle(anAngleSaver)
+      myLittleTurtle.jump(aPositionSaver.x, aPositionSaver.y)
+      myLittleTurtle.penDown
+      //right subtree
+      myLittleTurtle.turn(Random.between(-360, 360))
+      drawBranch(d - 1)
+    }
+    else {
+      myLittleTurtle.forward(20)
+
+    }
   }
 
 
   def drawTree(n:Int, length: Double): Unit = {
     if ( n > 1 ) {
-      drawBase(length)
+      drawBase(length-1)
       val anAngleSaver = myLittleTurtle.getTurtleAngle
       val aPositionSaver = myLittleTurtle.getPosition
-      myLittleTurtle.turn(-30-Random.between(-10,10))
+      myLittleTurtle.turn(Random.between(-90,90))
       //left subtree
-      drawTree(n - 1, 30+Random.between(-10,-1))
+      drawTree(n - 1, Random.between(10,30))
 
       myLittleTurtle.setAngle(anAngleSaver)
       myLittleTurtle.jump(aPositionSaver.x,aPositionSaver.y)
       myLittleTurtle.penDown
       //right subtree
-      myLittleTurtle.turn(30-Random.between(-10,10))
-      drawTree(n - 1, 30+Random.between(-10,-1))
+      myLittleTurtle.turn(Random.between(-90,90))
+      drawTree(n - 1, Random.between(10,30))
+
     }
     else {
-      drawBranch(3)
+      drawBranch(20)
     }
   }
 }
 
 object VeryNiceTree extends App with TreeDrawer {
 
-  drawTree(9,120)
+  drawTree(5,120)
 }
