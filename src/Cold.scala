052fa1dd4189693a90f8d85678b0e28de47b2322
capmars <capmars@gmail.com>
2023-05-08 20:06:40 +0200

    Level 2 Snowflake

--- src/Cold.scala
+++ src/Cold.scala
@@ -4,15 +4,42 @@ import hevs.graphics.TurtleGraphics
 trait aDrawer {
   val aColdTriangle = new TurtleGraphics(600, 600, "ColdTriangle")
 
-  def drawer(d:Double): Unit = {
-    aColdTriangle.turn(120)
-    aColdTriangle.forward(60)
+
+  def drawerReallyLittleTriangle(d:Double):Unit = {
 
   }
+  def drawerLittleTriangle(d:Double): Unit = {
+    if (d > 1){
+      aColdTriangle.turn(-60)
+      aColdTriangle.forward(20)
+      aColdTriangle.turn(120)
+      aColdTriangle.forward(20)
+    }
+  }
+  def drawerMainTriangle(d: Double): Unit = {
+    if (d > 0) {
+      aColdTriangle.forward(20)
+      drawerLittleTriangle(d-1)
+      aColdTriangle.turn(-60)
+      aColdTriangle.forward(20)
+      aColdTriangle.turn(120)
+      aColdTriangle.forward(20)
+      drawerLittleTriangle(d-1)
+      aColdTriangle.turn(-60)
+      aColdTriangle.forward(20)
+      aColdTriangle.turn(120)
+      aColdTriangle.forward(20)
+      drawerLittleTriangle(d-1)
+      aColdTriangle.turn(-60)
+      aColdTriangle.forward(20)
+    }
 
+
+  }
 }
-
 object ColdTriangle extends App with aDrawer {
+
+  drawerMainTriangle(3)
 
 
 
